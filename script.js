f0612da6939828a70b54e4f3e06f8fca9628f6a0
Ozzie <139321644+PickledChild@users.noreply.github.com>
2023-08-07 02:13:37 -0600

    add computer's choice in console log declaration

--- script.js
+++ script.js
@@ -17,18 +17,18 @@ function gameRun(choice) {
   if (choice === compChoice) {
     console.log("Tie!");
   } else if (choice === rock && compChoice === paper) {
-    console.log("You Lose!");
+    console.log("I chose Paper! You Lose!");
   } else if (choice === rock && compChoice === scissors) {
-    console.log("You Win!");
+    console.log("I chose Scissors! You Win!");
   } else if (choice === paper && compChoice === rock) {
-    console.log("You Win!");
+    console.log("I chose Rock! You Win!");
   } else if (choice === paper && compChoice === scissors) {
-    console.log("You Lose!");
+    console.log("I chose scissors! You Lose!");
   } else if (choice === scissors && compChoice === rock) {
-    console.log("You Lose!");
+    console.log("I chose Rock! You Lose!");
   } else if (choice === scissors && compChoice === paper) {
-    console.log("You Win!");
+    console.log("I chose Paper! You Win!");
   }
 }
 
-console.log(gameRun(paper));
+console.log(gameRun(rock));
