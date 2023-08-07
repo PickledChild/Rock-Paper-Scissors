bca089ba5ad9becc00270dc3e280afeed0da41af
Ozzie <139321644+PickledChild@users.noreply.github.com>
2023-08-07 01:25:54 -0600

    declare variables and created array for computers choice in game

---
+++ script.js
@@ -0,0 +1,17 @@
+// input for rock paper or scissors
+
+// line for console to pick randomly between rock
+// paper or scissors
+
+// line to compare input to random choice and see which one
+// wins
+
+// output win or lose
+
+let input = "";
+
+const rock = "Rock";
+const paper = "Paper";
+const scissors = "Scissors";
+
+let compChoice = [rock, paper, scissors][Math.floor(Math.random() * 3)];
