487ed078cb4baec5d843fb5c766213f57d63c0ff
Ozzie <139321644+PickledChild@users.noreply.github.com>
2023-08-17 13:59:24 -0600

    replace else if playRound function with switch function

--- script.js
+++ script.js
@@ -1,21 +1,81 @@
+const printAction = document.getElementById("results");
+const printPlayerScore = document.getElementById("playerScore");
+const printCompScore = document.getElementById("compScore");
+
+let playerScore = 0;
+let compScore = 0;
+
+printPlayerScore.textContent = `Your Score: ${playerScore}`;
+printCompScore.textContent = `My Score: ${compScore}`;
+
 // mechanics of game
+// function playRound(choice, compChoice) {
+//   if (choice === compChoice) {
+//     return (printAction.textContent = "Tie!");
+//   } else if (choice === "rock" && compChoice === "paper") {
+//     return (printAction.textContent = "I chose Paper! You Lose!");
+//   } else if (choice === "rock" && compChoice === "scissors") {
+//     return (printAction.textContent = "I chose Scissors! You Win!");
+//   } else if (choice === "paper" && compChoice === "rock") {
+//     return (printAction.textContent = "I chose Rock! You Win!");
+//   } else if (choice === "paper" && compChoice === "scissors") {
+//     return (printAction.textContent = "I chose Scissors! You Lose!");
+//   } else if (choice === "scissors" && compChoice === "rock") {
+//     return (printAction.textContent = "I chose Rock! You Lose!");
+//   } else if (choice === "scissors" && compChoice === "paper") {
+//     return (printAction.textContent = "I chose Paper! You Win!");
+//   } else choice == undefined;
+//   return (printAction.textContent = "Invalid entry. Try again!");
+// }
+
 function playRound(choice, compChoice) {
-  if (choice === compChoice) {
-    return (printAction.textContent = "Tie!");
-  } else if (choice === "rock" && compChoice === "paper") {
-    return (printAction.textContent = "I chose Paper! You Lose!");
-  } else if (choice === "rock" && compChoice === "scissors") {
-    return (printAction.textContent = "I chose Scissors! You Win!");
-  } else if (choice === "paper" && compChoice === "rock") {
-    return (printAction.textContent = "I chose Rock! You Win!");
-  } else if (choice === "paper" && compChoice === "scissors") {
-    return (printAction.textContent = "I chose Scissors! You Lose!");
-  } else if (choice === "scissors" && compChoice === "rock") {
-    return (printAction.textContent = "I chose Rock! You Lose!");
-  } else if (choice === "scissors" && compChoice === "paper") {
-    return (printAction.textContent = "I chose Paper! You Win!");
-  } else choice == undefined;
-  return (printAction.textContent = "Invalid entry. Try again!");
+  switch (choice) {
+    case "rock":
+      switch (compChoice) {
+        case "rock":
+          printAction.textContent = "Tie!";
+          break;
+        case "paper":
+          printAction.textContent = "I chose Paper! You Lose!";
+          compScore++;
+          break;
+        case "scissors":
+          printAction.textContent = "I chose Scissors! You Win!";
+          playerScore++;
+          break;
+      }
+      break;
+    case "paper":
+      switch (compChoice) {
+        case "rock":
+          printAction.textContent = "I chose Rock! You Win!";
+          playerScore++;
+          break;
+        case "paper":
+          printAction.textContent = "Tie!";
+          break;
+        case "scissors":
+          printAction.textContent = "I chose Scissors! You Lose!";
+          compScore++;
+          break;
+      }
+      break;
+    case "scissors":
+      switch (compChoice) {
+        case "rock":
+          printAction.textContent = "I chose Rock! You Lose!";
+          compScore++;
+          break;
+        case "paper":
+          printAction.textContent = "I chose Paper! You Win!";
+          playerScore++;
+          break;
+        case "scissors":
+          printAction.textContent = "Tie!";
+          break;
+      }
+      break;
+  }
 }
 
 // defines computers choice
@@ -26,26 +86,20 @@ function compInput() {
   return compChoice;
 }
 
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
-
 rock.addEventListener("click", () => {
   const playerChoice = "rock";
   const compChoice = compInput();
-  playRound(playerChoice, compChoice);
+  return playRound(playerChoice, compChoice);
 });
 
 paper.addEventListener("click", () => {
   const playerChoice = "paper";
   const compChoice = compInput();
-  playRound(playerChoice, compChoice);
+  return playRound(playerChoice, compChoice);
 });
 
 scissors.addEventListener("click", () => {
   const playerChoice = "scissors";
   const compChoice = compInput();
-  playRound(playerChoice, compChoice);
+  return playRound(playerChoice, compChoice);
 });
-
-const printAction = document.getElementById("results");
